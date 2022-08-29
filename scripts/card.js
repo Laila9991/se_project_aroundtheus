4a8d0d156a4573a3b9b455f6041480bcd5923416
Laila9991 <l.maghaouma@gmx.ch>
2022-08-29 11:24:54 +0200

    add handleTrashButton

--- scripts/card.js
+++ scripts/card.js
@@ -2,7 +2,7 @@ class Card{
 
     constructor(data, cardSelector ){
 
-    this.name = data.name;
+    this._name = data.name;
     this._link = data.link;
  
 
@@ -10,59 +10,65 @@ class Card{
 
 
 
+    this._cardLikeButton = null;
+  
+    this._cardImageButton = null;
+  
+    this._cardTrashButton = null;
+
+
     };
 
 
-
-  _handleLikeIcon = () => {
-    this._cardLikeButton.classList.toggle("card__button-like");
-  };
-
-  _handleTrashButton = () => {
-    this._element.remove();
-    this._element = null;
-  };
-
-  openPopUp = (data) => {
-    this.handleImageClick(data);
-  };
-    
-  _handlePreviewPicture() { 
-
-
-const imagePopup = document.querySelector("#content__popup");
-const imagePopupBackground = imagePopup.querySelector(".popup__image");
-const imagePopupTitle = imagePopup.querySelector(".popup__header");
-
-
-
-  imagePopupBackground.src= this._link;
-  imagePopupBackground.alt =  `Photo of ${this.link}`;
-  imagePopupBackground.textContent = this.name;
-  openImagePopup();
-
-
-
-
-  }
+   
 
 
     _setEventListeners() {
+      this._cardTrashButton = this._element.querySelector(".button_type_trash");
         this._cardTrashButton.addEventListener("click", () =>
-          this._handleTrashButton()
-        );
-    
+          this._handleTrashButton());
+        this._cardImageButton = this._element.querySelector(".button_type_image");
         this._cardImageButton.addEventListener("click", () =>
         this._handlePreviewPictur()
         );
-    
+        this._cardLikeButton = this._element.querySelector(".card__button-like");
         this._cardLikeButton.addEventListener("click", () =>
           this._handleLikeIcon()
         );
       }
     
 
+      _handleLikeIcon= ()=> {
+        this._cardLikeButton.classList.toggle("card__button-like");
+      };
+  
+  
     
+    _handleTrashButton=()=> {
+      this._element.remove();
+      this._element = null;
+    }
+  
+  
+    openPopUp = (data) => {
+      this.handleImageClick(data);
+    };
+      
+    _handlePreviewPicture(){ 
+  
+  
+  const imagePopup = document.querySelector("#content__popup");
+  const imagePopupBackground = imagePopup.querySelector(".popup__image");
+  const imagePopupTitle = imagePopup.querySelector(".popup__header");
+  
+  
+  
+    imagePopupBackground.src= this._link;
+    imagePopupBackground.alt =  `Photo of ${this.link}`;
+    imagePopupBackground.textContent = this.name;
+    openImagePopup(imagePopup);
+  
+    }
 
 
   
@@ -74,26 +80,19 @@ const imagePopupTitle = imagePopup.querySelector(".popup__header");
 
     generateCard = () => { 
     this._element = this._getTemplate();
-
+    this._setEventListeners();
 
     const cardImage = this._element.querySelector(".card__image");
     const cardHeader = this._element.querySelector(".card__text");
   
-    this._cardLikeButton = this._element.querySelector(".card__button-like");
   
-  
-    this._cardImageButton = this._element.querySelector(".button_type_image");
-    
-  
-    this._cardTrashButton = this._element.querySelector(".button_type_trash");
-
-    cardHeader.textContent = this._title;
+    cardHeader.textContent = this._name;
     cardImage.src = this._link;
-    cardImage.alt = `A Scenic Photo of ${this._title}`;
+    cardImage.alt = `A Scenic Photo of ${this._name}`;
 
 
 
-    this._setEventListeners();
+
 
     return this._element;
 
