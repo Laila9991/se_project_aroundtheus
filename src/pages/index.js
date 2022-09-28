fc3d6231b07bcdb1e51cafddb78178320e0d27c2
Laila9991 <l.maghaouma@gmx.ch>
2022-09-28 14:48:16 +0200

    git cache cleared

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,21 @@
 import "../pages/index.css";
-import {UserInfo} from "../components/UserInfo.js";
+import { UserInfo } from "../components/UserInfo.js";
 import { FormValidator } from "../components/FormValidator.js";
 import Card from "../components/Card.js";
-import {Section} from "../components/Section.js";
+import { Section } from "../components/Section.js";
 
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
 import {
   initialCards,
   editBtn,
-
   cardAddButton,
-
   profileNameInput,
   profileDescInput,
-
   config,
   selectors,
   addCardButton,
-} from "../utils/constants.js";
-
-
-
+} from "../utils/Constants.js";
 
 const createCard = (cardObject) => {
   const card = new Card(
@@ -36,20 +30,15 @@ const createCard = (cardObject) => {
   return card.generateCard();
 };
 
-
-
-
-
 function fillProfileForm() {
-  const { userName,  userJob } = userInfo.getUserInfo();
+  const { userName, userJob } = userInfo.getUserInfo();
 
-  profileNameInput.value = userName; 
-  profileDescInput.value = userJob; 
+  profileNameInput.value = userName;
+  profileDescInput.value = userJob;
 }
 
 const imagePopup = new PopupWithImage(selectors.imagePopup);
 imagePopup.setEventListeners();
-
 
 const cardSection = new Section(
   {
@@ -64,7 +53,6 @@ const cardSection = new Section(
 
 cardSection.renderItems();
 
-
 const addForm = new PopupWithForm("#add-popup", (data) => {
   const newCard = { name: data.title, link: data.link };
   const newCardEl = createCard(newCard);
@@ -78,24 +66,21 @@ addForm.setEventListeners();
 addCardButton.addEventListener("click", () => {
   addFormValidator.toggleButtonState();
   addForm.open();
-   });
-   console.log(addCardButton);
-
+});
+console.log(addCardButton);
 
 const addFormValidator = new FormValidator(config, selectors.cardForm);
 addFormValidator.enableValidation();
 
 const userInfo = new UserInfo(selectors);
 
-
-// corektur 
+// corektur
 const profileForm = new PopupWithForm(selectors.profilePopup, (data) => {
   userInfo.setUserInfo({
     userName: data.name,
-    userJob: data.descripton 
+    userJob: data.description,
   });
   profileForm.close();
-
 });
 
 profileForm.setEventListeners();
@@ -104,7 +89,6 @@ editBtn.addEventListener("click", () => {
   fillProfileForm();
   profileForm.open();
 });
-
 
 const editFormValidator = new FormValidator(config, selectors.profileForm);
 editFormValidator.enableValidation();
