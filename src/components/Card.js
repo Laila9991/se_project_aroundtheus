e2a5656a538053480de8a822eede6bde06daf219
Laila9991 <l.maghaouma@gmx.ch>
2022-10-30 16:12:24 +0100

    fixed likes card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 export class Card {
-  constructor({ data, handleCardClick, handleDeleteClick, handleLikeClick}, cardSelector) {
+  constructor(
+    { data, handleCardClick, handleDeleteClick, handleLikeClick },
+    cardSelector
+  ) {
     this._title = data.name;
     this._link = data.link;
-    this._likes= data.likes;
+    this._likes = data.likes;
     this._id = data._id;
     this._isLiked = false;
-    this._userId= data.currentUserId;
+    this._userId = data.currentUserId;
 
     if (this._likes.includes(this._userId)) {
       this._isLiked = true;
@@ -18,7 +21,6 @@ export class Card {
     this._handleDeleteClick = handleDeleteClick;
     this._handleLikeClick = handleLikeClick;
     this._ownerid = data.owner._id;
-
   }
 
   _getTemplate = () => {
@@ -42,33 +44,38 @@ export class Card {
     cardImage.alt = `A Scenic Photo of ${this._title}`;
 
     this._cardLikesCount.textContent = `${this._likes.length}`;
-    
+
     this._setEventListeners();
 
     return this._element;
   };
 
   updateLikes(res) {
-    this.likes = res.likes;
-    this._cardLikesCount.textContent = `${this.likes.length}`;
+    this._likes = res.likes;
+    this._cardLikesCount.textContent = `${this._likes.length}`;
     this._cardLikeButton.classList.toggle("card__button-like_filled");
+
+    this._likes.map((like) => {
+      if (like._id == this._userId) {
+        this._isLiked = true;
+      } else {
+        this._isLiked = false;
+      }
+    });
   }
-  
- //* _handleLikeClickIcon = () => {
-    //this._isLiked = !this._isLiked;
-   // if (this._isLiked) {
-   //   this._handleLikeClick(this._id);
-   // } else {
-   //   this._handleDeleteClick(this._id);
-    ///}
+
+  //* _handleLikeClickIcon = () => {
+  //this._isLiked = !this._isLiked;
+  // if (this._isLiked) {
+  //   this._handleLikeClick(this._id);
+  // } else {
+  //   this._handleDeleteClick(this._id);
+  ///}
   //};
 
   remove() {
     this._element.remove();
-  
   }
-
-  
 
   _checkLikeIcon = () => {
     for (let i = 0; i < this._likes.length; i++) {
@@ -78,7 +85,7 @@ export class Card {
         return;
       }
     }
-  }
+  };
 
   _checkCardOwnerId = () => {
     if (this._userId !== this._ownerid) {
@@ -87,23 +94,22 @@ export class Card {
     } else {
       return;
     }
-  }
+  };
 
   _setEventListeners() {
     this._cardTrashButton.addEventListener("click", () => {
       this._handleDeleteClick(this._id);
     });
     this._cardImageButton.addEventListener("click", () =>
-      this._handleCardClick({name: this._title, link: this._link })
+      this._handleCardClick({ name: this._title, link: this._link })
     );
 
     this._cardLikeButton.addEventListener("click", () =>
-      this._handleLikeClick(this._id, this._isLiked )
+      this._handleLikeClick(this._id, this._isLiked)
     );
 
     this._checkCardOwnerId();
     this._checkLikeIcon();
-    
   }
 }
 export default Card;
